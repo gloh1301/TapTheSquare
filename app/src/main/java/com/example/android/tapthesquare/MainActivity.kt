29ce28279b935aa3b616b0150812a0b38809d76b
Grant <grant_d_lohman@aol.com>
2023-02-14 15:50:58 -0600

    Square tapping app

---
+++ app/src/main/java/com/example/android/tapthesquare/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.android.tapthesquare
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+
+const val EXTRA_SQUARE_SIZE = "com.example.android.tapthesquare.SQUARE_SIZE"
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var seekBar: SeekBar
+    private lateinit var seekBarLabel: TextView
+    private lateinit var showSquareButton: Button
+
+    private val squareResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result -> handleSquareResult(result)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        seekBar = findViewById(R.id.seek_bar)
+        seekBarLabel = findViewById(R.id.seek_bar_label)
+        showSquareButton = findViewById(R.id.show_square_button)
+
+        val initialProgress = seekBar.progress
+        updateLabel(initialProgress)
+
+        seekBar.setOnSeekBarChangeListener( object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekbarComponent: SeekBar?, progress: Int, fromUser: Boolean) {
+                updateLabel(progress)
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+        showSquareButton.setOnClickListener {
+            showSquare()
+        }
+    }
+
+    private fun updateLabel(progress: Int) {
+        seekBarLabel.text = getString(R.string.seekbar_value_message, progress)
+    }
+    private fun showSquare() {
+        val showSquareIntent = Intent(this, SquareActivity::class.java)
+        showSquareIntent.putExtra(EXTRA_SQUARE_SIZE, seekBar.progress)
+        // startActivity(showSquareIntent)
+        squareResultLauncher.launch(showSquareIntent)
+    }
+    private fun handleSquareResult(result: ActivityResult) {
+        if (result.resultCode == RESULT_OK) {
+            val intent = result.data
+            val tapped = intent?.getBooleanExtra(EXTRA_TAPPED_INSIDE_SQUARE, false) ?: false
+            val message = if (tapped) {
+                getString(R.string.tapped_square)
+            } else {
+                getString(R.string.missed_square)
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        } else if (result.resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, getString(R.string.hit_back), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
